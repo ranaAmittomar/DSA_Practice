a7345b19fb6316dbc9fdb868401b02ee60492674
Amit Kumar Singh <120605843+ranaAmittomar@users.noreply.github.com>
2023-03-30 15:55:30 +0530

    Clone A LL having Random Pointers.. (LITTLE BIT COMPLEX CODE)

--- Linked_List_LevelUP.cpp
+++ Linked_List_LevelUP.cpp
@@ -2,12 +2,154 @@
 //
 
 #include <iostream>
+#include<unordered_map> //to include unordered_map -> for mappping the addresses in this question.
 using namespace std;
+
+//Clone a LL having random pointers.(Means->Same LL but with Different Addresses in memory than the original LL)
+
+class Node {
+
+    int data;
+    Node* next;
+    Node* random;
+public:
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+        random = NULL;
+    }
+
+    void setData(int d)
+    {
+        data = d;
+
+    }
+    int getData()
+    {
+        return data;
+    }
+    void setNext(Node* n)
+    {
+        next = n;
+    }
+    Node* getNext()
+    {
+        return next;
+    }
+    void setRandom(Node* r)
+    {
+        random = r;
+    }
+    Node* getRandom()
+    {
+        return random;
+    }
+
+};
+
+
+class LinkedList {
+
+    Node* head;
+    Node* tail;
+public:
+    LinkedList()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    Node* getHead()
+    {
+        return head;
+    }
+    Node* getTail()
+    {
+        return tail;
+    }
+    void insertAtTheEnd(int data)
+    {
+        Node* newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            Node* curr = head;
+            while (curr->getNext() != NULL)
+            {
+                curr = curr->getNext();
+            }
+            curr->setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+    void print()
+    {
+        Node* curr = head;
+        while (curr != NULL)
+        {
+            cout <<"("<<curr<<"," << curr->getData() << ") ("<<curr->getRandom()<< ", ";
+            if (curr->getRandom() != NULL)
+                cout << curr->getRandom()->getData()<<")" << endl;
+            else
+                cout << "NULL)" << endl;
+            curr = curr->getNext();
+        }
+    }
+
+};
+
+LinkedList* createClone(Node* head1)
+{
+    Node* curr, * next,*head2;
+    curr = head1;
+    unordered_map<Node*, Node*> um;
+    LinkedList ll2 = LinkedList();
+
+    while (curr != NULL)
+    {
+        ll2.insertAtTheEnd(curr->getData());
+        um[curr] = ll2.getTail();
+        curr = curr->getNext();
+    }
+    head2 = ll2.getHead();
+
+    while (head1 != NULL)
+    {
+        head2->setRandom(um[head1->getRandom()]);
+        head1 = head1->getNext();
+        head2 = head2->getNext();
+
+    }
+    return &ll2;
+}
 
 
 int main()
 {
+    LinkedList ll1= LinkedList();
+    ll1.insertAtTheEnd(10);
+    ll1.insertAtTheEnd(11);
+    ll1.insertAtTheEnd(12);
+    ll1.insertAtTheEnd(13);
 
+
+    Node* head1 = ll1.getHead();
+    head1->setRandom(head1->getNext()->getNext());
+    head1->getNext()->setRandom(NULL);
+    head1->getNext()->getNext()->setRandom(head1);
+    head1->getNext()->getNext()->getNext()->setRandom(head1->getNext());
+
+    ll1.print();
+    cout << endl;
+
+    cout << "LL2 Printint" << endl;
+
+    LinkedList* ll2 = createClone(head1);
+    ll2->print();
 
     return 0;
 }
